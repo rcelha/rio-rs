bc35fe38ae851b4563dc4ae1dc31ae1d1a4dce63
Rodrigo Corrêa <rcelha@gmail.com>
2021-12-13 09:27:07 +0000

    server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
+use tokio::net::{TcpListener, TcpStream};
+use tokio_stream::StreamExt;
+use tokio_util::codec::Framed;
+use tokio_util::codec::LinesCodec;
+
+async fn handle_client(stream: TcpStream) {
+    let mut lines = Framed::new(stream, LinesCodec::new());
+    while let Some(value) = lines.next().await {
+        println!("Got {}", value.unwrap());
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let addr = "0.0.0.0:5000";
+    let listener = TcpListener::bind(&addr).await.unwrap();
+    println!("Listening on: {}", addr);
     println!("main");
+    loop {
+        let (stream, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move { handle_client(stream).await });
+    }
 }
